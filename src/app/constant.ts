ed88e03b3b0f53970cc0e1abfb0f74322e353f54
miraj <miraj.ansari@sysfore.com>
2022-09-25 19:26:04 +0530

    [login]:login-services-added

---
+++ src/app/constant.ts
@@ -0,0 +1,26 @@
+
+const ApiEndpoint = 'http://localhost:3000';
+
+export const AppSettings = {
+    Urls: {
+
+        Account: {
+
+        },
+
+        User: {
+
+        },
+        process: {
+
+        },
+
+
+        Dashboard: {
+
+        },
+
+
+    },
+
+}
